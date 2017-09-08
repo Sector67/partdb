179f494399d1e4fe5b697fc2058edef29a7c8ad7
Liam <liam@cpan.org>
2017-09-08 15:23:23 -0500

    schema.sql: add category field, change drawer to a [text not null]

--- schema.sql
+++ schema.sql
@@ -1,7 +1,8 @@
 drop table if exists parts;
 create table parts (
     id integer primary key autoincrement,
-    drawer integer not null,
+    drawer text not null,
+    category text not null,
     partnum text not null,
     description text
 );
