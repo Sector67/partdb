0acd775501c40ef3c805b1f8a04faa9ffba4d047
Liam <liam@cpan.org>
2017-09-08 15:23:58 -0500

    flask initdb command should run data.sql to populate `parts`

--- partdb.py
+++ partdb.py
@@ -13,38 +13,48 @@ app.config.update(dict(
 ))
 app.config.from_envvar('PARTSDB_SETTINGS', silent=True)
 
+
 def connect_db():
     rv = sqlite3.connect(app.config['DATABASE'])
     rv.row_factory = sqlite3.Row
     return rv
+
 
 def get_db():
     if not hasattr(g, 'db'):
         g.db = connect_db()
     return g.db
 
+
 @app.teardown_appcontext
 def close_db(err):
     if hasattr(g, 'db'):
         g.db.close()
 
+
 def init_db():
     db = get_db()
     with app.open_resource('schema.sql', mode='r') as f:
         db.cursor().executescript(f.read())
+    with app.open_resource('data.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
     db.commit()
+
 
 def clean_name(some_var):
     return ''.join(char for char in some_var if char.isalnum())
+
 
 @app.cli.command('initdb')
 def initialize_db_cmd():
     init_db()
     print("Initialized the database")
 
+
 @app.route('/')
 def index_query():
     return render_template('iquery.html')
+
 
 @app.route('/parts/query')
 def part_query():
